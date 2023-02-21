f6a99f51c6a5a896f013472e99dbebe87b2ad811
Khalil <Khalilbentaiba@gmail.com>
2023-02-21 14:46:17 +0100

    Update style and Add header and footer

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const Game = (function () {
     function winner(board) {
         let win = "";
         if (
-            board[0][0] !== "" &&
+            (board[0][0] !== "") &&
             ((board[0][0] === board[0][1] && board[0][1] === board[0][2]) ||
                 (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
                 (board[0][0] === board[1][0] && board[1][0] === board[2][0]))
@@ -75,28 +75,25 @@ const Game = (function () {
             win = board[0][0];
         }
         if (
-            board[1][0] !== "" &&
-            (board[1][0] === board[1][1] &&
-            board[1][1] === board[1][2])
+            (board[1][0] !== "") &&
+            (board[1][0] === board[1][1] && board[1][1] === board[1][2])
         ) {
             win = board[1][0];
         }
         if (
-            board[2][0] !== "" &&
-            (board[2][0] === board[2][1] &&
-            board[2][1] === board[2][2])
+            (board[2][0] !== "") &&
+            (board[2][0] === board[2][1] && board[2][1] === board[2][2])
         ) {
             win = board[2][0];
         }
         if (
-            board[0][1] !== "" &&
-            (board[0][1] === board[1][1] &&
-            board[1][1] === board[2][1])
+            (board[0][1] !== "") &&
+            (board[0][1] === board[1][1] && board[1][1] === board[2][1])
         ) {   
             win = board[0][1];
         }
         if (
-            board[0][2] !== "" &&
+            (board[0][2] !== "") &&
             ((board[0][2] === board[1][2] && board[1][2] === board[2][2]) ||
                 (board[0][2] === board[1][1] && board[1][1] === board[2][0]))
         ) {
